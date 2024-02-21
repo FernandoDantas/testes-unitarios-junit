b77d1a6ce98eddc31c8b91b3af6f9eb2a8e904d7
Fernando Dantas <fernandojrsud@hotmail.com>
2024-02-21 14:24:34 -0300

    Testes com nested e beforeach

--- src/test/java/com/fernandojr/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/fernandojr/junit/utilidade/SaudacaoUtilTest.java
@@ -1,63 +1,40 @@
 package com.fernandojr.junit.utilidade;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import static com.fernandojr.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("Teste no utilitário de saudação")
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class SaudacaoUtilTest {
 
     @Test
-    @DisplayName("Deve saudar com bom dia")
-    public void saudarComBomDia() {
-        //Arrange - Variaveis que precisa preparar para executar seu cenário de teste
+    public void Dado_um_horario_matutino_Quando_saudar_Entao_deve_retornar_bom_dia() {
         int horaValida = 9;
-
-        //Act -Executando o cenário
-        String saudacao = saudar(horaValida);
-
-        //Assert - Asserções para verificar, validar se o cenário está correto ou não com o que se epsera
-        assertEquals("Bom dia", saudacao);
-    }
-
-    @Test
-    @DisplayName("Deve saudar com bom dia as 5 horas")
-    public void saudarComBomDiaAPartir5h() {
-        int horaValida = 5;
         String saudacao = saudar(horaValida);
         assertEquals("Bom dia", saudacao);
     }
 
     @Test
-    @DisplayName("Deve saudar com boa tarde")
-    public void saudarComBoaTarde() {
+    public void Dado_um_horario_vespertino_Quando_saudar_Entao_deve_retornar_boa_tarde() {
         int horaValida = 15;
         String saudacao = saudar(horaValida);
         assertEquals("Boa tarde", saudacao);
     }
 
     @Test
-    @DisplayName("Deve saudar com boa noite")
-    public void saudarComBoaNoite() {
-        int horaValida = 22;
-        String saudacao = saudar(horaValida);
-        assertEquals("Boa noite", saudacao);
-    }
-
-    @Test
-    @DisplayName("Deve saudar com boa noite as 4 horas")
-    public void saudarComBoaNoiteAs4h() {
+    public void Dado_um_horario_noturno_Quando_saudar_Entao_deve_retornar_boa_noite() {
         int horaValida = 4;
         String saudacao = saudar(horaValida);
         assertEquals("Boa noite", saudacao);
     }
 
     @Test
-    @DisplayName("Deve Lançar uma exceção")
-    public void deveLancarException() {
+    public void Dado_uma_hora_invalida_Quando_saudar_Entao_deve_lancar_exception() {
         int horaInvalida = -10;
         Executable chamadaInvalidaDeMetodo = () -> saudar(horaInvalida);
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, chamadaInvalidaDeMetodo);
@@ -65,12 +42,10 @@ class SaudacaoUtilTest {
     }
 
     @Test
-    @DisplayName("Não deve Lançar uma exceção")
-    public void naoDeveLancarException() {
+    public void Dado_uma_hora_valida_Quando_saudar_Entao_nao_deve_lancar_exception() {
         int horaValida = 0;
         Executable chamadaValidaDeMetodo = () -> saudar(horaValida);
-        assertDoesNotThrow(()-> chamadaValidaDeMetodo);
+        assertDoesNotThrow(chamadaValidaDeMetodo);
     }
-
 
 }
